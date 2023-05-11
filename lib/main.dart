cb1f928f331e06bad9b5fb08f24b0f4af1da9c6e
Nicolás Antonio <nicolasantonio@devneedzaio.com>
2023-05-11 03:08:27 -0500

    test lol

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_driver/driver_extension.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  enableFlutterDriverExtension();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
